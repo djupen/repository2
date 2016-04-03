17dde8896beafe2cefb880297f23ba71e02acc10
Vladkov Vladimir <a1401688@unet.univie.ac.at>
2016-04-03 13:34:42 +0200

    testen

--- src/tp/TestClass2.java
+++ src/tp/TestClass2.java
@@ -5,6 +5,7 @@ public class TestClass2 {
 	public static void main(String[] args) {
 		System.out.println("Hi ich teste mein 2. Repository!!!");
 		System.out.println("Commit and push teste ich jetzt");
+		System.out.println("Huhu, funktioniert das???");
 	}
 
 }
